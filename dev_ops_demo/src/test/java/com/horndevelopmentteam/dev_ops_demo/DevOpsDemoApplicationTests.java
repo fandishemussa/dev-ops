a038e5defd64d20515e2285599e08c45428447fb
fandishemussa <105553605+fandishemussa@users.noreply.github.com>
2026-06-27 22:35:08 +0300

    first commit

---
+++ dev_ops_demo/src/test/java/com/horndevelopmentteam/dev_ops_demo/DevOpsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horndevelopmentteam.dev_ops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
